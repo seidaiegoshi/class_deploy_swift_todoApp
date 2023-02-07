346bb0fea250d6acc44143242301c3e3cc89822a
seidaiegoshi <egoshi.seidai@gmail.com>
2023-02-07 20:13:45 +0900

    シンプルTODOアプリ完成

--- TodoApp/ContentView.swift
+++ TodoApp/ContentView.swift
@@ -8,37 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("memo") var savedMemo = "default memo"
-    @State var currentMemo = ""
-    let seasons = ["spring", "summer", "fall", "winter"]
+    @State var currentTodos: [Todo] = []
+    @State var input = ""
+    @AppStorage("todos") var todosData: Data  = Data()
+    
     var body: some View {
-        VStack {
+        VStack (spacing:0){
             HStack{
-                TextField("Memo",text:$currentMemo)
+                TextField("Todo",text:$input)
                     .padding()
                     .background(.white)
                     .cornerRadius(5)
                 
                 Button("Enter"){
-                    savedMemo = currentMemo
-                    currentMemo = ""
+                    do{
+                        try
+                        saveTodo(value: input)
+                        input = ""
+                    }
+                    catch{
+                        print(error.localizedDescription)
+                    }
                 }
                 .buttonStyle(.bordered)
                 .background(.blue)
                 .foregroundColor(.white)
                 .cornerRadius(5)
-                
             }
             .padding()
             .background(.yellow)
-            List(seasons,id:\.self){
-                season in Text(season)
+            List(currentTodos,id:\.id){
+                todo in Text(todo.value)
+            }
+        }
+        .onAppear{
+            do{
+                try currentTodos = fetchTodos()
+            }
+            catch{
+                print(error.localizedDescription)
             }
             
-            
         }
+        
+    }
+    
+    
+    func saveTodo(value:String) throws{
+        let todo = Todo(id: UUID(), value: value)
+        currentTodos.append(todo)
+        
+        let encodedTodos = try JSONEncoder().encode(currentTodos)
+        todosData = encodedTodos
+    }
+    
+    func fetchTodos() throws-> [Todo]{
+        try JSONDecoder().decode([Todo].self,from:todosData)
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ TodoApp/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  TodoApp
+//
+//  Created by 江越正大 on 2023/02/07.
+//
+
+import Foundation
+
+struct Todo:Codable{
+    let id: UUID
+    let value :String
+}
