f137853f3c69379e44968459d63c30a93e5479f4
seidaiegoshi <egoshi.seidai@gmail.com>
2023-02-07 18:46:22 +0900

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by 江越正大 on 2023/02/07.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
